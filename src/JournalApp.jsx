7189eaf1cd55a57c4b5e1d90972654f3c0cb6f8f
M0n00s <programacion.buffalo@gmail.com>
2023-02-27 13:37:36 -0400

    [ADD] material UI, configure the theme and config to use

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { LoginPage } from "./auth/pages";
 import { AppRouter } from "./router/AppRouter";
+import { AppTheme } from "./theme";
 
 export const JournalApp = () => {
   return (
     <>
       <BrowserRouter>
-        <AppRouter />
+        <AppTheme>
+          <AppRouter />
+        </AppTheme>
       </BrowserRouter>
     </>
   );
