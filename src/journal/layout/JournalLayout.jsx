5f90f5c9af678566831e123e45dfd75d1bfc287d
M0n00s <programacion.buffalo@gmail.com>
2023-03-03 12:08:24 -0400

    [UPDATE] navbar

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -7,7 +7,7 @@ const drawerWidth = 240;
 export const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display: "flex" }}>
-      {/* <Navbar /> */}
+      <Navbar drawerWidth={drawerWidth} />
       {/* SideBar */}
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         {children}
